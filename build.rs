7997e6e674ad8b1fc031eb85961882c52698afea
Vladimir Komendantskiy <komendantsky@gmail.com>
2018-04-18 21:23:39 +0100

    added a compile-time check for protoc in PATH

--- build.rs
+++ build.rs
@@ -1,10 +1,34 @@
 extern crate protoc_rust;
 
+use std::env;
+
+fn protoc_exists() -> bool {
+    let name = "PATH";
+    match env::var_os(name) {
+        Some(paths) => {
+            for path in env::split_paths(&paths) {
+                if path.join("protoc").exists() {
+                    return true;
+                }
+            }
+        }
+        None => println!("PATH environment variable is not defined.")
+    }
+    false
+}
+
 fn main() {
-    println!("cargo:rerun-if-changed=proto/message.proto");
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/proto",
-        input: &["proto/message.proto"],
-        includes: &["proto"],
-    }).expect("protoc");
+    if !protoc_exists() {
+        panic!("
+protoc cannot be found. Install the protobuf compiler in the \
+system path.");
+    }
+    else {
+        println!("cargo:rerun-if-changed=proto/message.proto");
+        protoc_rust::run(protoc_rust::Args {
+            out_dir: "src/proto",
+            input: &["proto/message.proto"],
+            includes: &["proto"],
+        }).expect("protoc");
+    }
 }
