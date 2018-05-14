50ba18b0dd1e9b4aa1a7cff6b8903324056b73aa
Vladimir Komendantskiy <komendantsky@gmail.com>
2018-05-14 11:50:00 +0100

    reduced the set of proposers to 1

--- tests/agreement.rs
+++ tests/agreement.rs
@@ -1,4 +1,7 @@
-//! Tests of the Binary Byzantine Agreement protocol.
+//! Tests of the Binary Byzantine Agreement protocol. Only one proposer instance
+//! is tested. Each of the nodes in the simulated network run only one instance
+//! of Agreement. This way we only test correctness of the protocol and not
+//! message dispatch between multiple proposers.
 //!
 //! There are three properties that are tested:
 //!
@@ -22,50 +25,40 @@ use hbbft::agreement::{Agreement, AgreementMessage};
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct NodeId(usize);
 
-/// Message pertaining to a particular Agreement instance.
-type InstanceMessage = (NodeId, AgreementMessage);
-type MessageQueue = VecDeque<InstanceMessage>;
+/// The queue of messages of a particular Agreement instance.
+type InstanceQueue = VecDeque<AgreementMessage>;
 
 struct TestNode {
     /// Sender ID.
     id: NodeId,
-    /// Map from proposer IDs into agreement instances.
-    agreements: BTreeMap<NodeId, Agreement<NodeId>>,
-    /// Queue of triples (sender_id, proposer_id, message).
-    queue: VecDeque<(NodeId, NodeId, AgreementMessage)>,
-    outputs: BTreeMap<NodeId, Vec<bool>>,
+    /// The only agreement instance.
+    agreement: Agreement<NodeId>,
+    /// Queue of tuples of a sender ID and a message.
+    queue: VecDeque<(NodeId, AgreementMessage)>,
+    /// All outputs
+    outputs: Vec<bool>,
 }
 
 impl TestNode {
-    fn new(id: NodeId, agreements: BTreeMap<NodeId, Agreement<NodeId>>) -> TestNode {
+    fn new(id: NodeId, agreement: Agreement<NodeId>) -> TestNode {
         TestNode {
             id,
-            agreements,
+            agreement,
             queue: VecDeque::new(),
-            outputs: BTreeMap::new(),
+            outputs: Vec::new(),
         }
     }
 
-    fn handle_message(&mut self) -> (Option<bool>, MessageQueue) {
-        let (sender_id, proposer_id, message) = self.queue
+    fn handle_message(&mut self) -> (Option<bool>, InstanceQueue) {
+        let (sender_id, message) = self.queue
             .pop_front()
             .expect("popping a message off the queue");
-        let (output, messages) = self.agreements
-            .get_mut(&proposer_id)
-            .unwrap()
+        let (output, messages) = self.agreement
             .handle_agreement_message(&sender_id, &message)
-            .map(|(output, messages)| {
-                // Annotate messages with the proposer ID.
-                let instance_messages = messages.into_iter().map(|m| (proposer_id, m)).collect();
-                (output, instance_messages)
-            })
             .expect("handling an agreement message");
         debug!("{:?} produced messages: {:?}", self.id, messages);
         if let Some(output) = output {
-            self.outputs
-                .entry(proposer_id)
-                .and_modify(|e| e.push(output))
-                .or_insert(vec![output]);
+            self.outputs.push(output);
         }
         (output, messages)
     }
@@ -79,31 +72,23 @@ struct TestNetwork {
 
 impl TestNetwork {
     fn new(num_nodes: usize) -> TestNetwork {
-        let make_node = |id: NodeId| {
-            let mut agreements = BTreeMap::new();
-
-            for i in 0..num_nodes {
-                agreements.insert(NodeId(i), Agreement::new(NodeId(i), num_nodes));
-            }
-            (id, TestNode::new(id, agreements))
-        };
-        let network = TestNetwork {
+        // Make a node with an Agreement instance associated with the proposer node 0.
+        let make_node = |id: NodeId| (id, TestNode::new(id, Agreement::new(NodeId(0), num_nodes)));
+        TestNetwork {
             nodes: (0..num_nodes).map(NodeId).map(make_node).collect(),
             scheduled_node_id: NodeId(0),
-        };
-        network
+        }
     }
 
-    fn dispatch_messages(&mut self, sender_id: NodeId, messages: MessageQueue) {
-        for (proposer_id, message) in messages {
-            for (id, node) in self.nodes.iter_mut() {
+    fn dispatch_messages(&mut self, sender_id: NodeId, messages: InstanceQueue) {
+        for message in messages {
+            for (id, node) in &mut self.nodes {
                 if *id != sender_id {
                     debug!(
                         "Dispatching from {:?} to {:?}: {:?}",
                         sender_id, id, message
                     );
-                    node.queue
-                        .push_back((sender_id, proposer_id, message.clone()));
+                    node.queue.push_back((sender_id, message.clone()));
                 }
             }
         }
@@ -139,16 +124,14 @@ impl TestNetwork {
         (sender_id, output)
     }
 
-    fn set_input(&mut self, sender_id: NodeId, proposer_id: NodeId, input: bool) {
+    fn set_input(&mut self, sender_id: NodeId, input: bool) {
         let message = self.nodes
-            .get_mut(&proposer_id)
+            .get_mut(&sender_id)
             .unwrap()
-            .agreements
-            .get_mut(&proposer_id)
-            .unwrap()
+            .agreement
             .set_input(input)
             .expect("set input");
-        self.dispatch_messages(sender_id, VecDeque::from(vec![(proposer_id, message)]));
+        self.dispatch_messages(sender_id, VecDeque::from(vec![message]));
     }
 }
 
@@ -158,13 +141,7 @@ fn test_agreement(mut network: TestNetwork) -> BTreeMap<NodeId, TestNode> {
     // Pick the first node with a non-empty queue.
     network.pick_node();
 
-    // Precompute the node IDs for checking loop termination.
-    let node_ids: Vec<NodeId> = network.nodes.keys().cloned().collect();
-
-    while network.nodes.values().any(|node| {
-        let output_ids: Vec<NodeId> = node.outputs.keys().cloned().collect();
-        output_ids != node_ids
-    }) {
+    while network.nodes.values().any(|node| node.outputs.is_empty()) {
         let (NodeId(id), output) = network.step();
         if let Some(value) = output {
             debug!("Node {} output {}", id, value);
@@ -179,18 +156,14 @@ fn test_agreement(mut network: TestNetwork) -> BTreeMap<NodeId, TestNode> {
 fn test_agreement_and_validity_with_1_faulty_node() {
     let mut network = TestNetwork::new(4);
 
-    for i in 0..4 {
-        network.set_input(NodeId(0), NodeId(i), true);
-        network.set_input(NodeId(1), NodeId(i), true);
-        network.set_input(NodeId(2), NodeId(i), true);
-        network.set_input(NodeId(3), NodeId(i), false);
-    }
+    network.set_input(NodeId(0), true);
+    network.set_input(NodeId(1), true);
+    network.set_input(NodeId(2), true);
+    network.set_input(NodeId(3), false);
 
     let nodes = test_agreement(network);
 
     for node in nodes.values() {
-        for proposer_id in node.agreements.keys() {
-            assert_eq!(node.outputs[proposer_id], vec![true]);
-        }
+        assert_eq!(node.outputs, vec![true]);
     }
 }
