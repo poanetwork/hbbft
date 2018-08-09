61833c7cd3e45c92b4fb844d540b2a0b59712e93
Marc Brinkmann <git@marcbrinkmann.de>
2018-08-09 14:23:29 +0200

    Remove some stale code.

--- tests/net_dynamic_hb.rs
+++ tests/net_dynamic_hb.rs
@@ -100,56 +100,6 @@ fn dyn_hb_test() {
     net.broadcast_input(&Input::Change(Change::Remove(NEW_OBSERVER_NODE_ID)))
         .expect("broadcasting failed");
 
-    // [
-
-    // fn has_remove(node: &TestNode<UsizeDhb>) -> bool {
-    //     node.outputs()
-    //         .iter()
-    //         .any(|batch| *batch.change() == ChangeState::Complete(Change::Remove(NodeUid(0))))
-    // }
-
-    // fn has_add(node: &TestNode<UsizeDhb>) -> bool {
-    //     node.outputs().iter().any(|batch| match *batch.change() {
-    //         ChangeState::Complete(Change::Add(ref id, _)) => *id == NodeUid(0),
-    //         _ => false,
-    //     })
-    // }
-
-    // // Returns `true` if the node has not output all transactions yet.
-    // let node_busy = |node: &TestNode<UsizeDhb>| {
-    //     if !has_remove(node) || !has_add(node) {
-    //         return true;
-    //     }
-    //     node.outputs().iter().flat_map(Batch::iter).unique().count() < num_txs
-    // };
-
-    // ]
-
-    // ??
-    // let mut input_add = false; // Whether the vote to add node 0 has already been input.
-
-    // enum TestNodeState {
-    //     WaitingForRemoval,
-    //     // v
-    //     // |
-    //     // v   input Add
-    //     // |
-    //     // v
-    //     WaitingForAdd,
-    //     // v
-    //     // |
-    //     // v   EPSILON
-    //     // |
-    //     // v
-    //     WaitingForTransactions,
-    //     // v
-    //     // |
-    //     // v   "terminate"
-    //     // |
-    //     // v
-    //     Done,
-    // }
-
     // We are tracking (correct) nodes' state through the process by ticking them off individually.
     let mut awaiting_removal: collections::BTreeSet<_> =
         net.correct_nodes().map(|n| n.id().clone()).collect();
@@ -161,21 +111,6 @@ fn dyn_hb_test() {
 
     // Run the network:
     loop {
-        //     // If a node is expecting input, take it from the queue. Otherwise handle a message.
-        //     let input_ids: Vec<_> = network
-        //         .nodes
-        //         .iter()
-        //         .filter(|(_, node)| {
-        //             node_busy(*node)
-        //                 && !node.instance().has_input()
-        //                 && node.instance().netinfo().is_validator()
-        //                 // If there's only one node, it will immediately output on input. Make sure we
-        //                 // first process all incoming messages before providing input again.
-        //                 && (network.nodes.len() > 2 || node.queue.is_empty())
-        //         })
-        //         .map(|(id, _)| *id)
-        //         .collect();
-
         let (node_id, step) = net.crank()
             .expect("network queue emptied unexpectedly")
             .expect("node failed to process step");
@@ -252,13 +187,6 @@ fn dyn_hb_test() {
             }
         }
 
-        // println!(
-        //     "Expect: {:?} ({:?}), R/A: {}/{}",
-        //     expected_outputs,
-        //     expected_outputs.values().all(|s| s.is_empty()),
-        //     awaiting_removal.len(),
-        //     awaiting_addition.len()
-        // );
         // Finally, check if we are done.
         if expected_outputs.values().all(|s| s.is_empty())
             && awaiting_addition.is_empty()
@@ -291,28 +219,7 @@ fn dyn_hb_test() {
                     .expect("could not send follow-up transaction");
             }
         }
-
-        //
     }
 
-    // // Handle messages in random order until all nodes have output all transactions.
-    // while network.nodes.values().any(node_busy) {
-    //     if let Some(id) = rng.choose(&input_ids) {
-    //         let queue = queues.get_mut(id).unwrap();
-    //         queue.remove_all(network.nodes[id].outputs().iter().flat_map(Batch::iter));
-    //         network.input(*id, Input::User(queue.choose(3, 10)));
-    //     }
-    //     network.step();
-    //     // Once all nodes have processed the removal of node 0, add it again.
-    //     if !input_add && network.nodes.values().all(has_remove) {
-    //         let pk = network.nodes[&NodeUid(0)]
-    //             .instance()
-    //             .netinfo()
-    //             .secret_key()
-    //             .public_key();
-    //         network.input_all(Input::Change(Change::Add(NodeUid(0), pk)));
-    //         input_add = true;
-    //     }
-    // }
-    // verify_output_sequence(&network);
+    // FIXME: Ensure output order is the same.
 }
