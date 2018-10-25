7df2d6f4a36c3bc0efc45612b8b71fc27a61531f
Marc Brinkmann <git@marcbrinkmann.de>
2018-10-25 16:51:58 +0200

    Fix typo in doctest.

--- src/traits.rs
+++ src/traits.rs
@@ -41,7 +41,7 @@ impl<S> SessionIdT for S where S: Display + Serialize + Send + Sync + Clone {}
 /// through `let _ = ...` or similar constructs, the implicit assumption should explicitliy be
 /// checked instead:
 ///
-/// ```rust,norun
+/// ```rust,no_run
 /// assert!(alg.propose(123).expect("Could not propose value").is_empty(),
 ///         "Algorithm will never output anything on first proposal");
 /// ```
