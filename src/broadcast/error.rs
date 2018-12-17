8689e6501eefeca37a743f59ef700efd95d09da9
Andreas Fackler <AndreasFackler@gmx.de>
2018-12-17 15:50:40 +0100

    Remove impossible Reed-Solomon errors.

--- src/broadcast/error.rs
+++ src/broadcast/error.rs
@@ -1,22 +1,11 @@
 use failure::Fail;
-use reed_solomon_erasure as rse;
 
 /// A broadcast error.
 #[derive(Clone, PartialEq, Debug, Fail)]
 pub enum Error {
-    /// Failed to create a `ReedSolomon` instance.
-    #[fail(display = "CodingNewReedSolomon error: {}", _0)]
-    CodingNewReedSolomon(#[cause] rse::Error),
-    /// Failed to encode the value.
-    #[fail(display = "CodingEncodeReedSolomon error: {}", _0)]
-    CodingEncodeReedSolomon(#[cause] rse::Error),
-    /// Failed to reconstruct the value.
-    #[fail(display = "CodingReconstructShardsReedSolomon error: {}", _0)]
-    CodingReconstructShardsReedSolomon(#[cause] rse::Error),
-    /// Failed to reconstruct the value.
-    // TODO: This should be unreachable.
-    #[fail(display = "CodingReconstructShardsTrivialReedSolomon error: {}", _0)]
-    CodingReconstructShardsTrivialReedSolomon(#[cause] rse::Error),
+    /// Due to a limitation in `reed_solomon_erasure`, only up to 256 nodes are supported.
+    #[fail(display = "Number of participants must be between 1 and 256")]
+    InvalidNodeCount,
     /// Observers cannot propose a value.
     #[fail(display = "Instance cannot propose")]
     InstanceCannotPropose,
